3c7965f67afc741173d581187687f491a44b2453
Jeremiasz Potoczny <ordinaryjeremaya@gmail.com>
2023-12-14 20:28:04 +0100

    finished get_trace function

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import warnings
 
 def extract_dataframe(set, type, terrain='Treadmill', sheet='Segment Position'):
     path = 'data/set{}/{}_Gait_{}.xlsx'.format(set, type, terrain)
     return pd.read_excel(path, sheet_name=sheet)
 
-df = extract_dataframe(set=4, type='Normal', terrain='Treadmill', sheet='Segment Position')
+df = extract_dataframe(set=5, type='Normal', terrain='Treadmill', sheet='Segment Position')
 
 human_blueprint = {
     'Spine': ('Pelvis', 'Neck', 'Head'),
@@ -17,25 +18,35 @@ human_blueprint = {
     'Right_Arm': ('Right Shoulder', 'Right Upper Arm', 'Right Forearm', 'Right Hand')
     }
 
+legs_blueprint = {
+    'Left_Leg': ('Pelvis', 'Left Upper Leg', 'Left Lower Leg', 'Left Foot', 'Left Toe'),
+    'Right_Leg': ('Pelvis', 'Right Upper Leg', 'Right Lower Leg', 'Right Foot', 'Right Toe'),
+}
+
+
 class Actor:
-    def __init__(self, df, axis=('x', 'z'), reference='Pelvis'):
+    def __init__(self, df, axis=('x', 'z'), reference='Pelvis', blueprint=human_blueprint):
         data = {}
         for segment in df.columns[:]:
             data[segment] = df[segment][1:].to_numpy()
         self.data = data
+        self.blueprint=blueprint
         self.coordinates = self.fill_blueprint(axis, reference)
     
-    def fill_blueprint(self, axis=('y', 'z'), reference='Pelvis'):
+    def fill_blueprint(self, axis, reference):
+
         """
         The 'coordinates' dictionary associates two values (one for 'x' and one for 'y' coordinates) 
         with each key representing a limb. 
         Each value is a 2D array, where each row corresponds to 'x' or 'y' coordinates 
         for the respective limb part at a given time.
+
+        Reverence is the body part that stays at coordinates (0, 0). Every other part is relative to it.
         """
     
         coordinates = {}
 
-        for limb, limb_tuple in human_blueprint.items():
+        for limb, limb_tuple in self.blueprint.items():
             limb_coordinates_x = np.transpose(
                 np.array([self.data['{} {}'.format(limb_part, axis[0])] - self.data['{} {}'.format(reference, axis[0])]
                           for limb_part in limb_tuple])
@@ -50,30 +61,30 @@ class Actor:
         return coordinates  
     
 
-    def get_trace(self, limb_part, blueprint=human_blueprint):
+    def get_trace(self, *limb_part):
         traces = {}
-        limb_part_found = False
+        parts_found = 0
         
-        for key in blueprint.keys():
+        for key in self.blueprint.keys():
             index = 0
-            for i in blueprint[key]:
-                if i == limb_part:
+            for i in self.blueprint[key]:
+                if i in limb_part:
                     x = np.transpose(self.coordinates[key][0])[index]
                     y = np.transpose(self.coordinates[key][1])[index]
-                    limb_part_found = True
-                    break
+                    traces[i] = (x, y)
+                    parts_found+=1
                 else:
-                    index += 1
-        
-        traces[limb_part] = (x, y)
+                    index += 1        
 
-        if limb_part_found:
+        if parts_found<len(limb_part):
+            warnings.warn("Not all limbs part are found and tracked. Check for spelling mistakes in 'get_trace' arguments")
+        if parts_found>0:
             return traces
         else:
             raise ValueError(f"Limb part '{limb_part}' not found in the blueprint")
 
-Human = Actor(df)
-traces = Human.get_trace('Right Toe')
+Human = Actor(df, blueprint=human_blueprint)
+traces = Human.get_trace('Right Toe', 'Left Hand')
 
 fig, ax = plt.subplots(figsize=(5, 5))
 ax.set_aspect('equal')
@@ -84,13 +95,10 @@ ax.set_ylim(-1, 1)
 body = {}
 for key in Human.coordinates.keys():
     body[key], = ax.plot([], [], 'o-', lw=2)
-    print(key)
 
 body_traces = {}
 for key in traces.keys():
     body_traces[key], = ax.plot([], [], '-', lw=1)
-
-
 
 def animate(i):
     for key in body.keys():
